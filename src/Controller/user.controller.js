7f1d656973eece48979f2a2713bfe1baa7afff16
faiyazShaikh <shaikhfaiyaz6353@gmail.com>
2026-03-31 12:01:30 +0530

    feat: add getUserChannelProfile endpoint to fetch user channel details with subscription info

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -388,3 +388,71 @@ export const changeCoverImage = asyncHandlerPromises(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, user, "CoverImage Updated Successfully.."));
 });
+
+const getUserChannelProfile = asyncHandlerPromises(async (req, res) => {
+  const { username } = req.params;
+  const userProfile = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        isSubscribed: 1,
+        subscribersCount: 1,
+        channelSubscribedToCount: 1,
+        avtar: 1,
+        email: 1,
+        coverImage: 1,
+        username: 1,
+      },
+    },
+  ]);
+
+  if (!userProfile.length) {
+    throw new ApiError(404, "channed does not exist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userProfile[0], "User channel fetched successfully"),
+    );
+});
