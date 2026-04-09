acca0c4dd035c0dcc797edb5091195b0d9bf8678
faiyazShaikh <shaikhfaiyaz6353@gmail.com>
2026-04-09 11:37:18 +0530

    created new api for add and delete comments

--- src/Controller/comment.controller.js
+++ src/Controller/comment.controller.js
@@ -19,7 +19,7 @@ export const getVideoComments = asyncHandlerPromises(async (req, res) => {
   const comments = await Comment.aggregate([
     {
       $match: {
-        video: videoId,
+        video: videoObjectId,
       },
     },
 
@@ -39,4 +39,77 @@ export const getVideoComments = asyncHandlerPromises(async (req, res) => {
   res
     .status(200)
     .json(new ApiResponse(200, comments, "Video Comment fetch Successfully.."));
+});
+
+export const addComment = asyncHandlerPromises(async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "Content is required to add comment..");
+  }
+
+  const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(404, "VideoId is not found in params");
+  }
+
+  const videoObjectId = new mongoose.Types.ObjectId(videoId);
+
+  const savedComment = await Comment.create({
+    content: content,
+    owner: req.user._id,
+    video: videoObjectId,
+  });
+
+  if (!savedComment) {
+    throw new ApiError(500, "Unable to add comment..");
+  }
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, savedComment, "Comment added successfully.."));
+});
+
+export const updateComment = asyncHandlerPromises(async (req, res) => {
+  const { commentId } = req.params;
+  const {content} = req.body;
+
+  if (commentId) {
+    throw new ApiError(400, "CommentId is Empty");
+  }
+
+  const exitingComment = await Comment.findOneById(commentId);
+
+  if (exitingComment.owner != req.user._id) {
+    throw new ApiError(400, "Access Denied you can't edit the comment");
+  }
+
+  const updatedCommnet = await Comment.findByIdAndUpdate({ id: commentId }, {content: content}, { new: true });
+
+  if(!updatedCommnet){
+    throw new ApiError(500, "Unable to update the comment");
+  }
+
+  res.status(200).json(new ApiResponse(200, updatedCommnet, "Comment updated successfully.."));
+  //update existing Comment,
+  //only owner of the comment can update,
+  //find comment by id and update,
+  //
+});
+
+export const deleteComment = asyncHandlerPromises(async (req, res) => {
+  const { commentId } = req.params;
+
+  if (commentId) {
+    throw new ApiError(400, "CommentId is Empty");
+  }
+
+  //find comment by id,
+  //check if the comment exist or not,
+  //only owner of the comment can delete,
+  //delete the comment,
+  //return response,
+
+  
 });
--- src/models/Comment.model.js
+++ src/models/Comment.model.js
@@ -18,7 +18,7 @@ const commentSchema = mongoose.Schema(
     },
   },
   {
-    timestaps: true,
+    timestamps: true,
   },
 );
 
